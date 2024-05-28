5b716509a21448feabb13a131058cdae37912fbc
mikolaj657 <161924077+mikolaj657@users.noreply.github.com>
2024-05-28 20:42:55 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,18 @@
+import geometry.Circle;
+import geometry.Point;
+
 public class Main {
     public static void main(String[] args) {
-        // Tworzenie obiektu klasy Point i inicjowanie jego wartości
-        Point point = new Point(3.5, 2.0);
+        // Tworzenie obiektu klasy Point
+        Point center = new Point(0, 0);
 
-        // Wyświetlanie współrzędnych punktu
-        System.out.println("Współrzędne punktu:");
-        System.out.println("x = " + point.getX());
-        System.out.println("y = " + point.getY());
+        // Tworzenie obiektu klasy Circle i inicjowanie jego wartości
+        Circle circle = new Circle(center, 5.0);
+
+        // Wyświetlanie właściwości koła
+        System.out.println("Właściwości koła:");
+        System.out.println("Środek koła: (" + circle.getCenter().getX() + ", " + circle.getCenter().getY() + ")");
+        System.out.println("Promień koła: " + circle.getRadius());
+        System.out.println("Obwód koła: " + circle.calculatePerimeter());
     }
 }
