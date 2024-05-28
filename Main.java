ef509fc77348fba482ff17201fc3506dd130520c
mikolaj657 <161924077+mikolaj657@users.noreply.github.com>
2024-05-28 20:42:24 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,19 @@
+import geometry.ColoredCircle;
+import geometry.Point;
+
 public class Main {
     public static void main(String[] args) {
-        // Tworzenie obiektu klasy Point i inicjowanie jego wartości
-        Point point = new Point(3.5, 2.0);
+        // Tworzenie obiektu klasy Point
+        Point center = new Point(0, 0);
 
-        // Wyświetlanie współrzędnych punktu
-        System.out.println("Współrzędne punktu:");
-        System.out.println("x = " + point.getX());
-        System.out.println("y = " + point.getY());
+        // Tworzenie obiektu klasy ColoredCircle i inicjowanie jego wartości
+        ColoredCircle coloredCircle = new ColoredCircle(center, 5.0, "blue");
+
+        // Wyświetlanie właściwości kolorowego koła
+        System.out.println("Właściwości kolorowego koła:");
+        System.out.println("Środek koła: (" + coloredCircle.getCenter().getX() + ", " + coloredCircle.getCenter().getY() + ")");
+        System.out.println("Promień koła: " + coloredCircle.getRadius());
+        System.out.println("Kolor koła: " + coloredCircle.getColor());
+        System.out.println("Obwód koła: " + coloredCircle.calculatePerimeter());
     }
 }
